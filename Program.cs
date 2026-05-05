951335a4cc429687a852f69d23aa8ffa15f38bfd
saras871 <Sara.hammaren@hotmail.com>
2026-05-05 19:06:47 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// ✅ Lägg till controllers
 builder.Services.AddControllers();
 builder.Services.AddScoped<GoalieRepository>();
 builder.Services.AddCors(options =>
@@ -16,7 +15,6 @@ var app = builder.Build();
 
 app.UseCors("AllowAll");
 
-// ✅ Aktivera controllers
 app.MapControllers();
 
 
